09d8efe2af446db00d9fbc42f60dfed9232b869a
suyeonKim1124 <nnnnnight@gmail.com>
2016-11-19 11:45:10 +0900

    Domain
    do

---
+++ src/main/java/com/jbc7276/spring/domain/Board.java
@@ -0,0 +1,18 @@
+package com.jbc7276.spring.domain;
+
+import java.util.Date;
+
+public class Board {
+	
+	private int bno;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regdate;
+	private int viewcnt;
+	
+	public Board() {
+		
+	}
+
+}
